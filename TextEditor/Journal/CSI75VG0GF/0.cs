9b81d25a5a92e4db32a8caef455cc8561eb9bb26
teddyzxcv <pan2691712848@gmail.com>
2021-02-01 21:55:31 +0300

    Bug fix

---
+++ TextEditor/Journal/CSI75VG0GF/0.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1.Семенар
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int y;
+            i = int.Parse(Console.ReadLine());
+            Console.WriteLine($"It is what it is {i * i}.....");
+            if (int.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine(y);
+            }
+            else
+            //kasdklajslkdjalkdjalkjdlakjsdlkajdlkjalkdjalkdjalksjdlkajsdlkasjslkdjalksjlks
+            {
+                Console.WriteLine("stop!! that ill{}egal!!");
+            }
+            int a = y > 5 ? 1 : 0;
+            Console.WriteLine(a);
+        }
+    }
+}
--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -14,9 +14,7 @@ namespace TextEditor
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+           
             Application.Run(new Form1());
         }
     }
